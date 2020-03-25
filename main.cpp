6850e8ddf725cb9383bb86226fa9a31b064ca89d
Kevin Buffardi <kbuffardi@csuchico.edu>
2020-03-25 11:36:30 -0700

    Simplify main

--- main.cpp
+++ main.cpp
@@ -7,14 +7,12 @@ using std::cout;
 
 int main()
 {
-    TopTenList* languages = new TopTenList("Programming Languages");
+    TopTenList* languages = new TopTenList();
     Hyperlink cpp;
 
     cpp.text = "C++";
     cpp.url = "https://www.cplusplus.com/";
     languages->set_at(1, cpp);
-
-    cout<<"Top Ten "<<languages->get_title()<<"\n";
 
     return 0;
 }
