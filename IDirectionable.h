1731abc5f706449bf94d1309f21d010a860bec8f
kbuffardi <kbuffardi@csuchico.edu>
2020-03-29 19:49:56 -0700

    Add IDirectionable interface

---
+++ IDirectionable.h
@@ -0,0 +1,12 @@
+#ifndef IDIRECTIONALBE_H
+#define IDIRECTIONABLE_H
+
+class IDirectionable
+{
+public:
+
+  virtual void display_forward() = 0;
+  virtual void display_backward() = 0;
+};
+
+#endif
